dd2fb36952f0aa16b9a5f8b3c50fbe8a842f7157
leedonggyu <zkfmapf123@naver.com>
2022-03-18 16:02:34 +0900

    2022-03-18 ~ 2022-03-19:

---
+++ 2022-03-18/index.ts
@@ -0,0 +1,16 @@
+/**
+ * input의 문자열들과 배열들을 거꾸로 만드시오
+ * 
+ * example) 
+ * input : "ABC"
+ * output : "BCA"
+ *
+ */
+
+// input
+const input_case_1 = 'ABC'
+const input_case_2 = "leedonggyu"
+const input_case_3 = [1,2,3,4,5]
+const input_case_4 = [1,'a',3,5,'c','f']
+
+// output
